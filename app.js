801c4f55cbcbea4698344b96f30db5aafe35a788
Aijamal <almazbekovaijamal06@gmail.com>
2024-10-12 09:44:13 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+/*
+
+1 git init
+2 git add README.md
+3 git commit -m "first commit"
+git branch -M main --------
+git remote add origin https://github.com/almazbekovaijamal/lesson-3.git
+git push -u origin master
+
+
+ctrl + j
+
+*/ 
